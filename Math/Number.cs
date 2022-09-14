d23a3a6f960b84da11242bed6418a5710acb3b6b
Вячеслав Кручин <vya4eslav.kruchin@yandex.ru>
2022-09-14 22:40:28 +0300

    Implemented the ability to extract numbers from an expression

---
+++ Math/Number.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Math
+{
+    /// <summary>
+    /// Целое число
+    /// </summary>
+    public class Number
+    {
+        public int Value { get; set; }
+
+        public Number(int value)
+        {
+            Value = value;
+        }
+
+        public Number(string value)
+        {
+            Value = int.Parse(value);
+        }
+    }
+}
